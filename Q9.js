9ba3bb8005a57569300c4d4cc04dd3d310d709ba
Asnababar <147834277+Asnababar@users.noreply.github.com>
2024-02-17 21:30:29 +0500

    Node.js Q9

---
+++ Q9.js
@@ -0,0 +1,5 @@
+"use strict";
+// Favourite number : store your favourite number in a variable then using that variable, create a meesage that reveals your favourite numner print that message.
+Object.defineProperty(exports, "__esModule", { value: true });
+let num = 6791;
+console.log(`My favourite number is ${num}`);
---
+++ Q9.ts
@@ -0,0 +1,5 @@
+// Favourite number : store your favourite number in a variable then using that variable, create a meesage that reveals your favourite numner print that message.
+
+let num : number = 6791
+
+console.log(`My favourite number is ${num}`);
